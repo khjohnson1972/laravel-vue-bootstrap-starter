9d1093b983d5a607aeca28313f97b23f8507fcbd
Kevin Johnson <kevin@amplitudedesign.com>
2020-04-02 16:30:58 -0700

    Work on form submission.

--- resources/js/main/components/Contact.vue
+++ resources/js/main/components/Contact.vue
@@ -4,8 +4,27 @@
         <form
             method="post"
             novalidate
-            @submit="checkForm"
+            @submit.prevent="submit"
         >
+            <!-- Name -->
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input
+                    id="name"
+                    v-model="$v.formData.name.$model"
+                    type="text"
+                    class="form-control"
+                    :class="{ 'is-invalid': attemptSubmit && $v.formData.name.$invalid, 'is-valid': attemptSubmit && !$v.formData.name.$invalid }"
+                    placeholder="Enter name"
+                >
+                <div
+                    v-if="!$v.formData.name.required && attemptSubmit"
+                    class="invalid-feedback"
+                >
+                    This field is required
+                </div>
+            </div>
+
             <!-- Email -->
             <div class="form-group">
                 <label for="email">Email address</label>
@@ -36,13 +55,31 @@
                     class="form-text text-muted"
                 >We'll never share your email with anyone else.</small>
             </div>
-
             <button
                 type="submit"
                 class="btn btn-primary"
+                :disabled="submitStatus === 'PENDING'"
             >
                 Submit
             </button>
+            <p
+                v-if="submitStatus === 'OK'"
+                class="typo__p"
+            >
+                Thanks for your submission!
+            </p>
+            <p
+                v-if="submitStatus === 'ERROR'"
+                class="typo__p"
+            >
+                Please fill the form correctly.
+            </p>
+            <p
+                v-if="submitStatus === 'PENDING'"
+                class="typo__p"
+            >
+                Sending...
+            </p>
         </form>
     </div>
 </template>
@@ -54,46 +91,39 @@ export default {
         return {
             attemptSubmit: false,
             formData: {
-                email: ''
-            }
+                email: '',
+                name: ''
+            },
+            submitStatus: null
         }
     },
     validations: {
         formData: {
-            email: { required, email }
+            email: { required, email },
+            name: { required }
         }
     },
     methods: {
-        checkForm: function (e) {
+        submit: function (e) {
             e.preventDefault()
             this.attemptSubmit = true
 
-            if (this.name && this.phone && this.company && this.email && this.message) {
-                return true
-            }
+            this.$v.$touch()
 
-            this.errors = {}
-
-            if (!this.email) {
-                this.missingEmail = true
-                this.errors.email = 'Please enter your email.'
-            }
-            if (!this.name) {
-                this.errors.name = 'Please enter your name.'
-            }
-            if (!this.phone) {
-                this.errors.phone = 'Please enter your phone.'
-            }
-            if (!this.company) {
-                this.errors.company = 'Please enter your company.'
-            }
-            if (!this.message) {
-                this.errors.message = 'Please provide some message.'
+            if (this.$v.$invalid) {
+                this.submitStatus = 'ERROR'
+            } else {
+                // do your submit logic here
+                this.submitStatus = 'PENDING'
+                setTimeout(() => {
+                    this.submitStatus = 'OK'
+                    this.clearForm()
+                }, 500)
             }
         },
-        onSubmit: function (e) {
-            e.preventDefault()
-            alert('here')
+        clearForm: function () {
+            this.formData.name = ''
+            this.formData.email = ''
         }
     }
 }
