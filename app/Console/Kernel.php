849b9e92b98bd9d1a67e44e8b55800f4c1103564
Kevin Johnson <kevin@amplitudedesign.com>
2020-04-08 23:22:43 +0000

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,8 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule){
+    protected function schedule(Schedule $schedule)
+    {
         // $schedule->command('inspire')->hourly();
     }
 
