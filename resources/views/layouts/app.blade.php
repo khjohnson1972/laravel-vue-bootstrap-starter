5f1a7336c1b7c2d6b9d826d955381d56be620a7e
Kevin Johnson <kevin@amplitudedesign.com>
2020-04-14 22:46:50 +0000

    Add the named routes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                                        Dashboard
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('admin.contact.index') }}">
+                                        Contacts
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
