cc5d2c943fad8f6b20dcfa81ab7f910d7586cde1
Kevin Johnson <kevin@amplitudedesign.com>
2020-04-19 17:35:18 +0000

    Add the button

--- resources/views/admin/contacts/edit.blade.php
+++ resources/views/admin/contacts/edit.blade.php
@@ -3,16 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        {{ $contact->name }}
-        <form method="post" action="{{ route('admin.contact.update', $contact->id) }}">
-            <!--hidden-->
-            <input name="_token" type="hidden" value="{{ csrf_token() }}" />
-            <input name="_method" type="hidden" value="put" />
-            <input name="name" type="text" class="form-control {{ $errors->hasAny(['name']) ? 'is-invalid' : '' }}" id="name" placeholder="Name" value="{{ old('name', $contact->name) }}" />
-            <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+        <div class="col">
+            <form id="edit-contact"method="post" action="{{ route('admin.contact.update', $contact->id) }}">
+                <!--hidden-->
+                <input name="_token" type="hidden" value="{{ csrf_token() }}" />
+                <input name="_method" type="hidden" value="put" />
 
-            <input type="submit"/>
-        </form>
+                <!--name-->
+                <div class="form-group row">
+                    <label for="name" class="col-12 col-sm-2 form-control-label">Name</label>
+                    <div class="col-12 col-sm-10">
+                        <input name="name" type="text" class="form-control {{ $errors->hasAny(['name']) ? 'is-invalid' : '' }}" id="name" placeholder="Name" value="{{ old('name', $contact->name) }}" />
+                        <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col text-right">
+                        <button name="submit" type="submit" id="submit" form="edit-contact" class="btn btn-success">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
