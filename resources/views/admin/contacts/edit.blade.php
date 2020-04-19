27896c0ed97645b768bf8c7ac10ad8b16f766cf9
Kevin Johnson <kevin@amplitudedesign.com>
2020-04-19 17:28:24 +0000

    Test out rules.

--- resources/views/admin/contacts/edit.blade.php
+++ resources/views/admin/contacts/edit.blade.php
@@ -8,7 +8,9 @@
             <!--hidden-->
             <input name="_token" type="hidden" value="{{ csrf_token() }}" />
             <input name="_method" type="hidden" value="put" />
-            <input type="text" name="name" value="{{ old('name', $contact->name) }}"/>
+            <input name="name" type="text" class="form-control {{ $errors->hasAny(['name']) ? 'is-invalid' : '' }}" id="name" placeholder="Name" value="{{ old('name', $contact->name) }}" />
+            <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+
             <input type="submit"/>
         </form>
     </div>
