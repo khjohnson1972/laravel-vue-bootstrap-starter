14a9538219f6803a310d4b2ceb1e58e9ebc703cb
Kevin Johnson <kevin@amplitudedesign.com>
2020-04-20 15:40:12 -0700

    Add tests

---
+++ resources/js/admin/components/Alert.unit.js
@@ -0,0 +1,47 @@
+import Alert from './Alert.vue'
+
+describe('Alert.vue Component', () => {
+  let wrapper = null
+
+  // creates a new wrapper after each test
+  beforeEach(() => {
+    wrapper = shallowMount(Alert, {
+      propsData: {
+        alert: {
+          message: 'test'
+        }
+      }
+    })
+  })
+
+  // destroy the wrapper after each test
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
+  test('exports a valid component', () => {
+    expect(Alert).toBeAComponent()
+  })
+
+  test('test shown returns message', () => {
+    expect(wrapper.vm.shown).toBe('test')
+  })
+
+  test('test shown returns title', () => {
+    wrapper.setProps({
+      alert: {
+        title: 'title'
+      }
+    })
+    expect(wrapper.vm.shown).toBe('title')
+  })
+
+  test('test status returns class name', () => {
+    wrapper.setProps({
+      alert: {
+        context: 'success'
+      }
+    })
+    expect(wrapper.vm.status).toBe('alert-success')
+  })
+})
