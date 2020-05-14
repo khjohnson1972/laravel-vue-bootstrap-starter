91d4064039e9ce029b81e56ddc87193a898b2e2b
Kevin Johnson <kevin@amplitudedesign.com>
2020-05-14 12:09:08 -0700

    Remove babel-jest.

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/resources/js/tests/unit/custom-matchers'],
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
     '.+\\.(css|scss|jpe?g|png|gif|webp|svg|mp4|webm|ogg|mp3|wav|flac|aac|woff2?|eot|ttf|otf)$':
       'jest-transform-stub'
